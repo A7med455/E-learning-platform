7d301f4a09dfda84f2e835d43ef618c209716aab
Abdelrahman <abdelrahman.tarek2007@gmail.com>
2026-05-07 20:15:32 +0300

    getProfile.php

---
+++ php/getProfile.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+include '../php/db.php';
+include '../php/session_guard.php';
+
+header('Content-Type: application/json');
+
+$user_id = $_SESSION['user_id'];
+
+$query = "SELECT id, fname, lname, email, age, role FROM users WHERE id = $user_id";
+$result = mysqli_query($conn, $query);
+
+if (mysqli_num_rows($result) === 1) {
+    $user = mysqli_fetch_assoc($result);
+    echo json_encode(['success' => true, 'data' => $user]);
+} else {
+    echo json_encode(['success' => false, 'message' => 'User not found']);
