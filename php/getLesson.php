f3d25d64c72bed4f27ff7c40d57f6b1d9aeabbae
HabibaaMohammed <habibabassam315@gmail.com>
2026-05-08 01:14:05 +0300

    getLesson.php edited

--- php/getLesson.php
+++ php/getLesson.php
@@ -1,34 +1,44 @@
 <?php
+
 include 'db.php';
 include 'session_guard.php';
 
-header('Content-Type : application/json');
+header('Content-Type: application/json');
 
-$user_id=$_SESSION['user_id'];
-$course_id=$_GET['course_id'] ?? 0;
+$user_id = $_SESSION['user_id'];
 
-//check enrollment
-$check=mysqli_query($conn,"
-SELECT *FROM enrollments WHERE user_id='$user_id' AND course_id='$course_id'
-");
- if(mysqli_num_rows($check)==0){
+$course_id = $_GET['course_id'] ?? 0;
+
+
+// CHECK IF STUDENT IS ENROLLED
+$check = mysqli_query($conn,
+    "SELECT * FROM enrollments
+     WHERE user_id = '$user_id'
+     AND course_id = '$course_id'"
+);
+
+if (mysqli_num_rows($check) == 0) {
     echo json_encode([
-        'success'=>false,
-        'message'=>'you are not enrolled in this course'
+        'success' => false,
+        'message' => 'You are not enrolled in this course'
     ]);
-    exit;
- }
- // get lessons
- $result=mysqli_query($conn,"
- SELECT * FROM lessons WHERE course_id='$course_id'
- ");
+    exit();
+}
 
- $lessons=[];
+// GET LESSONS
+$result = mysqli_query($conn,
+    "SELECT * FROM lessons
+     WHERE course_id = '$course_id'"
+);
+$lessons = [];
+while ($row = mysqli_fetch_assoc($result)) {
 
- while($row=mysqli_fetch_assoc($result)){
-    $lessons[]=$row;
- }
- echo json_encode([
-    'success'=>true,
-    'message'=>$lessons
- ]);
+    $lessons[] = $row;
+}
+// RETURN JSON RESPONSE
+echo json_encode([
+    'success' => true,
+    'data' => $lessons
+]);
+
+?>
