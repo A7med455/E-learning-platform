604f67788271ba2394ec5fe13ddc3d8dd266d636
Ahmed Sameh <AhmedSameh2006ASF@gmail.com>
2026-05-08 18:52:22 +0300

    get_balance.php Updated

--- php/get_balance.php
+++ php/get_balance.php
@@ -1,5 +1,4 @@
 <?php
-
 include 'db.php';
 include 'session_guard.php';
 
@@ -8,22 +7,17 @@ header("Content-Type: application/json");
 $user_id = $_SESSION['user_id'];
 
 $sql = "SELECT balance FROM wallets WHERE user_id = '$user_id'";
-
-$result = mysqli_query($conn , $sql);
+$result = mysqli_query($conn, $sql);
 
 if ($row = mysqli_fetch_assoc($result)) {
-
     echo json_encode([
         'success' => true,
         'balance' => $row['balance']
     ]);
-
 } else {
-
     echo json_encode([
         'success' => false,
         'message' => 'Wallet not found'
     ]);
 }
-
 ?>
